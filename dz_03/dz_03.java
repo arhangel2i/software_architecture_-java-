ca5caea09ee25e8aeff6244baf577488df18937d
arhangel2i <arhangel2i@mail.ru>
2022-11-07 18:54:37 +0300

    add dz_03

---
+++ dz_03/dz_03.java
@@ -0,0 +1,71 @@
+package dz_03;
+import dz_01.Color;
+
+public class dz_03 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+}
+
+enum TypeCar{
+    SEDAN,
+    PICKUP
+}
+enum TypeFuel{
+    GASOLIN,
+    DISEL
+}
+enum TypeGearBox{
+    MT,
+    AT
+}
+
+abstract class Car{
+    private String make;
+    private String model;
+    private Color color;
+    private TypeCar bodyType;
+    private int numberWheels;
+    private TypeFuel fuelType;
+    private TypeGearBox gearboxType;
+    private double engineCapacity;
+
+    public void movement(){}
+    public void maintenance(){}
+    public boolean gearShifting(){return true;}
+    public boolean switchHeadlights(){return true;}
+    public boolean switchWipers(){return true;}
+
+
+    public void setMake(String make) {this.make=make;}
+    public String getMake() {return make;}
+
+    public void setModel(String model) {this.model=model;}
+    public String getModel() {return model;}
+
+    public void setColor(Color color) {this.color=color;}
+    public Color getColor() {return color;}
+
+    public void setBodyType(TypeCar bodyType) {this.bodyType=bodyType;}
+    public TypeCar getBodyType() {return bodyType;}
+
+    public void setNumberWheels(int numberWheels) {this.numberWheels=numberWheels;}
+    public int getNumberWheels() {return numberWheels;}
+
+    public void setFuelType(TypeFuel fuelType) {this.fuelType=fuelType;}
+    public TypeFuel getFuelType() {return fuelType;}
+
+    public void setGearboxType(TypeGearBox gearboxType) {this.gearboxType=gearboxType;}
+    public TypeGearBox getGearboxType() {return gearboxType;}
+    
+    public void setEngineCapacity(double engineCapacity) {this.engineCapacity=engineCapacity;}
+    public double getEngineCapacity() {return engineCapacity;}
+}
+
+class PickUp extends Car{
+    private int loadCapacity;
+
+    public int getLoadCapacity() {return loadCapacity;}
+    public void setLoadCapacity(int loadCapacity){this.loadCapacity=loadCapacity;}
+}
